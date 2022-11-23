193b4b1620935d848ff9c831ad073188cd03ef1e
noe <noe.breton@etu.univ-tours.fr>
2022-11-23 03:12:05 +0200

    add animation button

--- Assets/Scripts/UI/ToggleAnimation.cs
+++ Assets/Scripts/UI/ToggleAnimation.cs
@@ -1,22 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Linq;
 
 
 public class ToggleAnimation : MonoBehaviour
 {
     ToggleGroup toggleGroup;
+    [SerializeField] List<Toggle> Toggles;
+
     private void Start()
     {
-        toggleGroup = GetComponent<ToggleGroup>();
-    }
 
+    }
+    private void OnEnable()
+    {
+
+        Debug.Log("AAAAAAAAAAAAAAAa");
+
+        toggleGroup = GetComponent<ToggleGroup>();
+        foreach (Toggle toggle in Toggles)
+        {
+            toggle.GetComponent<Animator>().SetBool("On", toggle.isOn);
+            if (toggle.isOn)
+            {
+                toggle.GetComponent<Animator>().SetTrigger("On1");
+            }
+        }
+    }
     // Start is called before the first frame update
     // Update is called once per frame
-    void SetAnimation()
+    public void SetAnimation()
     {
-        //toggleGroup.GetFirstActiveToggle().GetComponent<Animator>().SetBool("On", true);
+        if (EventSystem.current.currentSelectedGameObject != null && EventSystem.current.GetComponent<Animator>())
+        {
+
+            Toggle currentToogle = EventSystem.current.currentSelectedGameObject.GetComponent<Toggle>();
+            bool currentState = currentToogle.GetComponent<Animator>().GetBool("On");
+            currentToogle.GetComponent<Animator>().SetBool("On", currentToogle.isOn);
+            if (currentToogle.isOn)
+            {
+                currentToogle.GetComponent<Animator>().SetTrigger("On1");
+            }
+        }
+        foreach (Toggle toggle in Toggles)
+        {
+            toggle.GetComponent<Animator>().SetBool("On", toggle.isOn);
+        }
+
+        // toggleGroup.GetFirstActiveToggle().GetComponent<Animator>().SetTrigger("On");
 
     }
 }
